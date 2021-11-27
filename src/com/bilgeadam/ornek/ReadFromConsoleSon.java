b6733fba96fc54868b89b3cb0810eb9b69348a20
samet.coruk <sametcoruk0604@gmail.com>
2021-11-27 14:29:45 +0300

    ReadFromCodeSon yaş yanlış girilince tekrar soracak hale getirildi.

--- src/com/bilgeadam/ornek/ReadFromConsoleSon.java
+++ src/com/bilgeadam/ornek/ReadFromConsoleSon.java
@@ -1,47 +1,72 @@
 package com.bilgeadam.ornek;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ReadFromConsoleSon {
 
     String isim;
     int yas;
+    static String isim;
+    static int yas;
 
     Scanner sc = new Scanner(System.in);
 
     public String isimSor()
     {
+        String ad = "";
         System.out.println("İsminizi giriniz:");
-        isim = sc.nextLine();
+        System.out.print("İsminizi giriniz:");
+        ad = sc.nextLine();
 
-        return isim;
+        return ad;
     }
 
     public int yasSor()
     {
+        int localYas = 0;
         System.out.println("Yaşınızı giriniz:");
-        yas = sc.nextInt();
+        localYas = sc.nextInt();
 
-        return yas;
+        return localYas;
+        try {
+            int localYas = 0;
+            System.out.print("Yaşınızı giriniz:");
+            localYas = Integer.parseInt(sc.nextLine());
+
+            return localYas;
+        }
+        catch(NumberFormatException e)
+        {
+            System.err.println("Hata: Yaş alanına sayı girmek zorundasınız!");
+            System.out.println();
+            return -99;
+        }
+
     }
 
     public void sonucYazdir()
     {
         if (yas <= 35)
-            System.out.println("Yolun 1. yarısındasın" + isim);
+            System.out.println("Yolun 1. yarısındasın " + isim);
         else
-            System.out.println("Yolun 2. yarısındasın" + isim);
+            System.out.println("Yolun 2. yarısındasın " + isim);
     }
-
-
-
-
-
     public static void main(String[] args) {
+        ReadFromConsoleSon son = new ReadFromConsoleSon();
+
+        son.isimSor();
+        son.yasSor();
+        isim = son.isimSor();
+        yas = son.yasSor();
+
+        while(yas < 0)
+        {
+            yas = son.yasSor();
+        }
+
+        son.sonucYazdir();
+
 
     }
-
-
-
-
 }
